147f4d83eddf2247746b553752cb88f52f55deba
Erick Vargas <erickkvargas@gmail.com>
2022-04-06 15:32:47 -0700

    commit tree bf8ed9e26a2e1f856b61f18cd3e4de54e360e45b

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+//function findMatching(collection,driver){
+    //for (const user of collection){
+        //if (user.name === driver){
+        //return (user.name)
+       // }
+    //}
+//}
+//findMatching(drivers,"bobby")
+
+//const { stringContaining } = require("expect");
+
+function findMatching(drivers, stng){
+   let  theName= drivers.filter(driver => driver === stng || driver === stng.toLowerCase())
+   return theName;
+}
+function fuzzyMatch(drivers, string) {
+    let name=drivers.filter(driver => driver.slice(0,string.length) === string)
+    return name;
+}
+function matchName (drivers,string){
+let names = drivers.filter(driver =>driver.name === string)
+return names;
+}
